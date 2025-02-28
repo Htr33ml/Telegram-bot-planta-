68c77d9d1ce6d1640d54e24c0f4cabc92eebb9f0
Htr33ml <hugotremmel22@gmail.com>
2025-02-28 00:34:39 -0300

    Update keepalive.js

--- keepalive.js
+++ keepalive.js
@@ -1,14 +1,14 @@
 // keepalive.js
 const express = require("express");
-const server = express();
+const app = express();
 
-server.all("/", (req, res) => {
+app.all("/", (req, res) => {
   res.send("Bot is running! All good here.");
 });
 
 function keepAlive() {
-  server.listen(3000, () => {
-    console.log("Server is ready to keep Repl alive!");
+  app.listen(3000, () => {
+    console.log("Server is ready to keep the app alive!");
   });
 }
 
